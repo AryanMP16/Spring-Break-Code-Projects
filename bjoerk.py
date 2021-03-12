c3f79986e13e93f659f68a46398d8825997bdb2f
AryanMP16 <43282996+AryanMP16@users.noreply.github.com>
2021-03-12 10:50:04 -0800

    Made basic reply command

--- bjoerk.py
+++ bjoerk.py
@@ -1,12 +1,18 @@
-# This example requires the 'members' privileged intents
-
 import discord
 from discord.ext import commands
 import random
 
-clinet = commands.Bot(command_prefix = '~')
-@clinet.event
-async def on_ready():
+class MyClient(discord.Client):
+    async def on_ready():
     print('Initialized')
+
+    async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('bjoerk'):
+            await message.reply('I have been summoned')
+
+clinet = commands.Bot(command_prefix = '~')
 
 client.run('ODE5OTk4MTEwMDEyNTM4OTEx.YEuwwA.e5uGtcFsPi5AHTZDsIEQPsR1o84')
