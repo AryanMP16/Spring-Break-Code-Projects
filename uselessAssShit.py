fdd2150f5efdb305c7e698582d54843895898e8d
AryanMP16 <43282996+AryanMP16@users.noreply.github.com>
2021-04-29 16:05:02 -0700

    Update uselessAssShit.py

--- uselessAssShit.py
+++ uselessAssShit.py
@@ -1,8 +1,11 @@
+import time
 i=0
+dumbTime = 0
 while True:
+    dumbTime+=10
     if i==0:
         i+=1
-        print(i)
     elif i==1:
         i-=1
-        print(i)
+    print(i, "   Time(s): ", (dumbTime/1000))
+    time.sleep(0.01)
